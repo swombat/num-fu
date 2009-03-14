745ff8a02da04503ddf521b18e23f82402bd970f
Daniel Tenner <danieltenner@daniel-tenners-macbook-pro.local>
2009-03-14 23:55:00 +0000

    More fixes

--- lib/num_fu/upload_extractor.rb
+++ lib/num_fu/upload_extractor.rb
@@ -4,7 +4,7 @@ module NumFu
     # Determine whether the upload is via Nginx Upload Module or not, and if so, transmogrify it to be
     # in a standard form that can be passed on to InstanceMethods#uploaded_data=
     def extract_upload(name="file")
-      if params["#{name}.path"]
+      if params["#{name}"] && params["#{name}"]["uploaded_data"] && params["#{name}"]["uploaded_data"][".path"]
         fake_upload_data = FakeUploadData.new
         fake_upload_data.path = params["#{name}"]["uploaded_data"][".path"]
         fake_upload_data.original_filename = params["#{name}"]["uploaded_data"][".name"]
